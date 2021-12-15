ce98b15894862ad1eb06f34a1c08358ded38a3f8
vikash kumar <vkvikashkumar987@gmail.com>
2021-12-15 23:03:15 +0530

    pipe added

--- flappy.py
+++ flappy.py
@@ -1,9 +1,26 @@
-import pygame, sys
+import pygame, sys , random
 
 def draw_floor():
     screen.blit(floor_surface,(floor_x_pos,700))
     screen.blit(floor_surface,(floor_x_pos+576,700))
 
+def create_pipe():
+    random_pipe_pos=random.choice(pipe_height)
+    bottom_pipe=pipe_surface.get_rect(midtop=(700,random_pipe_pos))
+    top_pipe = pipe_surface.get_rect(midbottom=(700,random_pipe_pos-300))
+    return top_pipe,bottom_pipe
+def move_pipes(pipes):
+    for pipe in pipes:
+        pipe.centerx-=5
+    return pipes
+
+def draw_pipes(pipes):
+    for pipe in pipes:
+        if pipe.bottom >=840:
+            screen.blit(pipe_surface,pipe)
+        else:
+            flip_pipe=pygame.transform.flip(pipe_surface,False,True)
+            screen.blit(flip_pipe,pipe)
 pygame.init()
 screen=pygame.display.set_mode((576,840))
 clock=pygame.time.Clock()
@@ -22,6 +39,13 @@ bird_surface=pygame.transform.scale2x(bird_surface)
 bird_rect=bird_surface.get_rect(center=(100,350))
 
 
+pipe_surface=pygame.image.load('assets/pipe-green.png')
+pipe_surface=pygame.transform.scale2x(pipe_surface)
+pipe_list=[]
+SPAWNPIPE=pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE,1200)
+pipe_height=[400,600,800]
+
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -31,15 +55,23 @@ while True:
             if event.key==pygame.K_SPACE:
                 bird_movement=0
                 bird_movement-=12
+        if event.type==SPAWNPIPE:
+            pipe_list.extend(create_pipe())
+
     screen.blit(bg_surface,(0,-150))
 
     bird_movement+=gravity
     bird_rect.centery+=bird_movement
-
     screen.blit(bird_surface,bird_rect)
+
+    pipe_list=move_pipes(pipe_list)
+    draw_pipes(pipe_list)
+
     floor_x_pos -= 1
     draw_floor()
     if floor_x_pos<=-576:
         floor_x_pos=0
+
+    
     pygame.display.update()
     clock.tick(120)
