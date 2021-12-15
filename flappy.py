e85d366338cc6e775ad30e0579bacdfe698ff824
vikash kumar <vkvikashkumar987@gmail.com>
2021-12-15 15:08:28 +0530

    add background and frontfloor

--- flappy.py
+++ flappy.py
@@ -1,3 +1,28 @@
-import pygame
+import pygame, sys
+
+def draw_floor():
+    screen.blit(floor_surface,(floor_x_pos,700))
+    screen.blit(floor_surface,(floor_x_pos+576,700))
+
+
+
 pygame.init()
-screen=pygame.display.set_mode((576,1024))
+screen=pygame.display.set_mode((576,840))
+clock=pygame.time.Clock()
+
+bg_surface=pygame.transform.scale2x(pygame.image.load('assets/background-day.png').convert())
+floor_surface=pygame.image.load('assets/base.png').convert()
+floor_surface=pygame.transform.scale2x(floor_surface)
+floor_x_pos=0
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.blit(bg_surface,(0,-150))
+    floor_x_pos -= 1
+    draw_floor()
+    if floor_x_pos<=-576:
+        floor_x_pos=0
+    pygame.display.update()
+    clock.tick(120)
