3b406e4e471188466ec7816ce65d507014ff2021
vikash kumar <vkvikashkumar987@gmail.com>
2021-12-16 15:16:13 +0530

    collision done for rect

--- flappy.py
+++ flappy.py
@@ -3,7 +3,6 @@ import pygame, sys , random
 def draw_floor():
     screen.blit(floor_surface,(floor_x_pos,700))
     screen.blit(floor_surface,(floor_x_pos+576,700))
-
 def create_pipe():
     random_pipe_pos=random.choice(pipe_height)
     bottom_pipe=pipe_surface.get_rect(midtop=(700,random_pipe_pos))
@@ -13,7 +12,6 @@ def move_pipes(pipes):
     for pipe in pipes:
         pipe.centerx-=5
     return pipes
-
 def draw_pipes(pipes):
     for pipe in pipes:
         if pipe.bottom >=840:
@@ -21,12 +19,23 @@ def draw_pipes(pipes):
         else:
             flip_pipe=pygame.transform.flip(pipe_surface,False,True)
             screen.blit(flip_pipe,pipe)
+def check_collision(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            return False
+    if bird_rect.top <= -100 or bird_rect.bottom >= 900:
+        return False
+    return True 
+
+
+
 pygame.init()
 screen=pygame.display.set_mode((576,840))
 clock=pygame.time.Clock()
 
 gravity=0.25
 bird_movement=0
+game_active=True
 
 bg_surface=pygame.transform.scale2x(pygame.image.load('assets/background-day.png').convert())
 
@@ -52,20 +61,27 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_SPACE:
+            if event.key==pygame.K_SPACE and game_active:
                 bird_movement=0
                 bird_movement-=12
+            if event.key==pygame.K_SPACE and game_active==False:
+                game_active=True
+                pipe_list.clear()
+                bird_rect.center=(100,350)
+                bird_movement=0
         if event.type==SPAWNPIPE:
             pipe_list.extend(create_pipe())
 
     screen.blit(bg_surface,(0,-150))
 
-    bird_movement+=gravity
-    bird_rect.centery+=bird_movement
-    screen.blit(bird_surface,bird_rect)
+    if game_active:
+        bird_movement+=gravity
+        bird_rect.centery+=bird_movement
+        screen.blit(bird_surface,bird_rect)
+        game_active=check_collision(pipe_list)
 
-    pipe_list=move_pipes(pipe_list)
-    draw_pipes(pipe_list)
+        pipe_list=move_pipes(pipe_list)
+        draw_pipes(pipe_list)
 
     floor_x_pos -= 1
     draw_floor()
