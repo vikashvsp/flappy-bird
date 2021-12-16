f33f4633aa103be19d7b78d589a391a15498c7a8
vikash kumar <vkvikashkumar987@gmail.com>
2021-12-16 17:29:40 +0530

    adding score done

--- flappy.py
+++ flappy.py
@@ -33,14 +33,38 @@ def bird_animation():
     new_bird=bird_frames[bird_index]
     new_bird_rect=new_bird.get_rect(center=(100,bird_rect.centery))
     return new_bird,new_bird_rect
+def score_display(game_state):
+    if game_state=='main_game':
+        score_surface=game_font.render(str(int(score)),True,(255,255,255))
+        score_rect=score_surface.get_rect(center=(288,100))
+        screen.blit(score_surface,score_rect)
+    if game_state=='game_over':
+        score_surface=game_font.render(f'Score: {int(score)}',True,(255,255,255))
+        score_rect=score_surface.get_rect(center=(288,100))
+        screen.blit(score_surface,score_rect)
 
+
+        high_score_surface=game_font.render(f'High Score: {int(score)}',True,(255,255,255))
+        high_score_rect=high_score_surface.get_rect(center=(288,650))
+        screen.blit(high_score_surface,high_score_rect)
+def update_score(score,high_score):
+    if score > high_score:
+        high_score=score
+    return high_score
 pygame.init()
 screen=pygame.display.set_mode((576,840))
 clock=pygame.time.Clock()
+game_font=pygame.font.Font('04B_19.ttf',40)
+
+
 
 gravity=0.25
 bird_movement=0
 game_active=True
+score=0
+high_score=0
+
+
 
 bg_surface=pygame.transform.scale2x(pygame.image.load('assets/background-day.png').convert())
 
@@ -87,6 +111,7 @@ while True:
                 pipe_list.clear()
                 bird_rect.center=(100,350)
                 bird_movement=0
+                score=0
         if event.type==SPAWNPIPE:
             pipe_list.extend(create_pipe())
 
@@ -108,6 +133,12 @@ while True:
 
         pipe_list=move_pipes(pipe_list)
         draw_pipes(pipe_list)
+
+        score += 0.01
+        score_display('main_game')
+    else:
+        high_score=update_score(score,high_score)
+        score_display('game_over')
 
     floor_x_pos -= 1
     draw_floor()
